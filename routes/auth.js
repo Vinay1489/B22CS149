aaf2f5311ecf6c57ecead51d44b2678af48b1bc3
Vinay Vennampally <vinayvennampally@gmail.com>
2025-09-08 10:49:53 +0530

    Sign up or registering of user completed Successfully

---
+++ routes/auth.js
@@ -0,0 +1,58 @@
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+
+const router=Router();
+
+router.post("/register",async(req,res)=>
+{
+    try{
+        const {mail,password}=req.body;
+        const hashedpassword=await bcrypt.hash(password,10);
+        const user=new User({mail,password:hashedpassword});
+        await user.save();
+        res.status(201).json({
+            message:"User registered Successfully",
+            user
+        });
+    }
+    catch(err)
+    {
+        res.status(400).json({
+            error:err.message,
+        })
+    }
+});
+
+router.post("/login",async(req,res)=>
+{
+    try{
+        const{mail,password}=req.body;
+        const user=await User.findOne({mail});
+        if(!user) return res.status(404).json({
+            error:"User not found",
+        });
+
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch) return res.status(400).json({
+            error:"Invalid Passwrod",
+        })
+
+        const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1h"});
+        res.status(200).json({
+            message:"User logged in Successfully",
+            token
+        })
+    }
+    catch(err)
+    {
+        res.status(500).json(
+            {
+                error:err.message,
+            }
+        )
+    }
+})
+
+export default router;
