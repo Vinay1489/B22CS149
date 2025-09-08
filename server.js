888a3b420753f2b63b01788f1002189ceb7fe75c
Vinay Vennampally <vinayvennampally@gmail.com>
2025-09-08 10:33:04 +0530

    Server connected Successfully

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import {connectDB} from "./db.js";
+
+
+
+
+dotenv.config();
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(rateLimit({windowMs:1000*60,max:100}));
+
+
+
+app.get("/check",(req,res)=>
+{
+    res.json(
+        {
+            status:"OK server is running fine",
+        }
+    )
+});
+
+const PORT=process.env.PORT||4000;
+app.listen(PORT,async()=>
+{
+    await connectDB();
+    console.log(`Server running on port ${PORT}`);
+});
